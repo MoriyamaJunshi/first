1ec9d620250d6bc0cf815f2a6d91d99b0bf73941
moriyama junshi <junshi614@gmail.com>
2023-08-28 23:17:55 +0900

    課題6

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_160655) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_082127) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
